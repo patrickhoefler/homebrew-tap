b0336d72f95128bc831d8c7af48e3858a6ee0b8f
goreleaserbot <goreleaser@carlosbecker.com>
2022-04-14 14:08:52 +0200

    Brew formula update for dockerfilegraph version v0.7.1

--- dockerfilegraph.rb
+++ dockerfilegraph.rb
@@ -5,23 +5,31 @@
 class Dockerfilegraph < Formula
   desc "Visualize your multi-stage Dockerfile"
   homepage "https://github.com/patrickhoefler/dockerfilegraph"
-  version "0.7.0"
+  version "0.7.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/patrickhoefler/dockerfilegraph/releases/download/v0.7.0/dockerfilegraph_0.7.0_darwin_amd64.tar.gz"
-      sha256 "e25589ab1ee43ce29f558e443e02c6f8750ee2ee274cb8a2f9c6656af800ea6f"
+    url "https://github.com/patrickhoefler/dockerfilegraph/releases/download/v0.7.1/dockerfilegraph_0.7.1_darwin_amd64.tar.gz"
+    sha256 "18b0b9aa3e26f008c21b476c36e6bc3b7a26b817100586f3cf1dfdb20a01455d"
 
-      def install
-        bin.install "dockerfilegraph"
+    def install
+      bin.install "dockerfilegraph"
+    end
+
+    if Hardware::CPU.arm?
+      def caveats
+        <<~EOS
+          The darwin_arm64 architecture is not supported for the Dockerfilegraph
+          formula at this time. The darwin_amd64 binary may work in compatibility
+          mode, but it might not be fully supported.
+        EOS
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/patrickhoefler/dockerfilegraph/releases/download/v0.7.0/dockerfilegraph_0.7.0_linux_amd64.tar.gz"
-      sha256 "460e484c0b73e308706533caa3eb06eeff092784c33fb038947927dc0688cd5f"
+      url "https://github.com/patrickhoefler/dockerfilegraph/releases/download/v0.7.1/dockerfilegraph_0.7.1_linux_amd64.tar.gz"
+      sha256 "628c41b95907faabf89346e8a36ce3e4ee48fec30efe24fda4a9081f398ea77d"
 
       def install
         bin.install "dockerfilegraph"
